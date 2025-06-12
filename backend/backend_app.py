f6bea5ce7a5f789bec3e7f6174037fcd67f4ba9d
unRekable <unRekable@protonmail.com>
2025-06-12 20:24:43 +0200

    feat(add) added API endpoint to POST a new title

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+from urllib import request
+
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +12,22 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
-def get_posts():
-    return jsonify(POSTS)
+@app.route('/api/posts', methods=['GET', 'POST'])
+def posts():
+    if request.method == 'POST':
+        if not request.json or not 'title' in request.json or not 'content' in request.json:
+            return jsonify({"error": "Missing title or content"}), 400
+
+        new_id = max([post['id'] for post in POSTS]) + 1
+        title = request.json['title']
+        content = request.json['content']
+
+        new_post = {"id": new_id, "title": title, "content": content}
+        POSTS.append(new_post)
+
+        return jsonify(new_post), 201
+    else:
+        return jsonify(POSTS)
 
 
 if __name__ == '__main__':
